005ce03bd1761f7ae879a3c09b712c79c7c752b5
wischi2k <bswischi@gmx.net>
2025-12-15 08:54:29 +0100

    chore: remove final root debug scripts

--- clear_icon.py
+++
@@ -1,6 +0,0 @@
-import sqlite3
-con = sqlite3.connect('cs2_prices.sqlite')
-con.execute("UPDATE items SET icon_url=NULL WHERE market_hash LIKE '%Neo-Noir%'")
-con.commit()
-con.close()
-print('✓ Icon gelöscht')
